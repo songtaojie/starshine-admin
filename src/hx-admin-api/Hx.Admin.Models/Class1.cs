7050f946c47e20742f83264b6b7db9dfe43eb516
songtaojie <stjworkemail@163.com>
2023-12-21 22:08:57 +0800

    添加服务

---
+++ src/hx-admin-api/Hx.Admin.Models/Class1.cs
@@ -0,0 +1,11 @@
+﻿// MIT License
+//
+// Copyright (c) 2021-present songtaojie, Daming Co.,Ltd and Contributors
+//
+// 电话/微信：song977601042
+
+namespace Hx.Admin.Models;
+public class Class1
+{
+
+}
---
+++ src/hx-admin-api/Hx.Admin.Repository/Class1.cs
@@ -0,0 +1,11 @@
+﻿// MIT License
+//
+// Copyright (c) 2021-present zuohuaijun, Daming Co.,Ltd and Contributors
+//
+// 电话/微信：18020030720 QQ群1：87333204 QQ群2：252381476
+
+namespace Hx.Admin.Repository;
+public class Class1
+{
+
+}
