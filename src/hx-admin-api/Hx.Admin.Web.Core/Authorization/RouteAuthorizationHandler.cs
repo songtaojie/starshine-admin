383f78345e7e0009def19d901234ad769e1c27e2
songtaojie <stjworkemail@163.com>
2024-01-02 22:13:16 +0800

    添加授权策略

--- src/hx-admin-api/Hx.Admin.Web.Core/Authorization/RouteAuthorizationHandler.cs
+++ src/hx-admin-api/Hx.Admin.Web.Core/Authorization/RouteAuthorizationHandler.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
