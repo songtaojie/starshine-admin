0a074faad784b5c0199cf05f227bbe28210fcbc7
宋涛杰 <stjworkemail@163.com>
2024-01-03 19:25:27 +0800

    修改Program

--- src/hx-admin-api/Hx.Admin.Web.Entry/Program.cs
+++ src/hx-admin-api/Hx.Admin.Web.Entry/Program.cs
@@ -1,3 +1,5 @@
+using AspNetCoreRateLimit;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using Serilog.Events;
@@ -13,6 +15,11 @@ try
     Log.Information("Starting web application");
 
     var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddControllersWithViews()
+        .ConfigureApiBehaviorOptions(options =>
+        {
+
+        });
     builder.Services.AddHxAdminOptions();
     builder.Services.AddCache(builder.Configuration);
     builder.Services.AddSqlSugar();
@@ -21,6 +28,18 @@ try
         .ReadFrom.Services(services)
         .Enrich.FromLogContext()
         .WriteTo.Console());
+    // 配置Nginx转发获取客户端真实IP
+    // 注1：如果负载均衡不是在本机通过 Loopback 地址转发请求的，一定要加上options.KnownNetworks.Clear()和options.KnownProxies.Clear()
+    // 注2：如果设置环境变量 ASPNETCORE_FORWARDEDHEADERS_ENABLED 为 True，则不需要下面的配置代码
+    builder.Services.Configure<ForwardedHeadersOptions>(options =>
+    {
+        options.ForwardedHeaders = ForwardedHeaders.All;
+        options.KnownNetworks.Clear();
+        options.KnownProxies.Clear();
+    });
+    // 限流服务
+    builder.Services.AddInMemoryRateLimiting();
+    builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
 
     var app = builder.Build();
 
