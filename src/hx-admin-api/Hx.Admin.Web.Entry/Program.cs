11c268e1a2c1b25e747fa57cba2d61c17317db1b
songtaojie <stjworkemail@163.com>
2024-01-29 23:04:45 +0800

    添加日志配置

--- src/hx-admin-api/Hx.Admin.Web.Entry/Program.cs
+++ src/hx-admin-api/Hx.Admin.Web.Entry/Program.cs
@@ -1,29 +1,28 @@
-using Hx.Admin.Serilog;
-using Serilog;
-using Serilog.Events;
+using NLog;
+using NLog.Web;
+using System;
 
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-    .Enrich.FromLogContext()
-    .WriteTo.Console()
-    .CreateBootstrapLogger();
 
+// 在构建主机之前，早期初始化NLog以允许启动和异常日志
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 try
 {
-    Log.Information("Starting web application");
+    logger.Info("Starting web application");
     var builder = WebApplication.CreateBuilder(args);
     builder.ConfigureHxWebApp();
     builder.Services.AddAdminCoreService(builder.Configuration);
-    builder.Host.UseSerilogSetup();
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
     var app = builder.Build();
     app.UseAdminCoreApp(builder.Environment);
     app.Run();
 }
 catch (Exception ex)
 {
-    Log.Fatal(ex, "Application terminated unexpectedly");
+    logger.Error(ex, "程序因异常而停止");
 }
 finally
 {
-    Log.CloseAndFlush();
+    // 确保在应用程序退出之前刷新和停止内部计时器/线程(避免Linux上的分段错误)
+    NLog.LogManager.Shutdown();
 }
